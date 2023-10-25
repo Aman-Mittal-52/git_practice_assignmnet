e5e6bed79ba199a37a2e24f257864e465900b0ce
Aman <amanmittle1234@gmail.com>
2023-10-25 19:23:27 +0530

    Prime number code

---
+++ prime Num problem.js
@@ -0,0 +1,13 @@
+let count = 3;
+let num = 13;
+for(i=0; i >= num; i++){
+    if(num%5==0){
+        count++
+    }
+}
+if(count==6){
+    console.log("prime")
+}
+else if(num==5) {
+    console.log("Not prime")
+}
